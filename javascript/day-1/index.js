9b0952fde9cd7ee6693fc56f6b293b8ae848ace2
Anuj Prakash Dixit <anujprakashdixit@gmail.com>
2025-10-02 00:59:16 +0530

    random function

---
+++ javascript/day-1/index.js
@@ -0,0 +1,8 @@
+let myname = "Rohit";
+const account = 1234;
+
+
+console.log(myname);
+
+//primitive data type immutable
+// non primitive data types mutable
---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+let b = 12.3535345;
+
+console.log(typeof(b.toFixed(2)));
+
+function random(){
+    
+}
